9859edaa06ec716a346eea373c54adcd0d4247b2
vermenea <vermenea@gmail.com>
2025-03-04 17:52:02 +0100

    refactor: changed structure of the website and added some styling to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,24 @@
-import Link from "next/link";
-import React from "react";
-import Particles from "./components/particles";
-
-const navigation = [
-  { name: "Projects", href: "/projects" },
-  { name: "Contact", href: "/contact" },
-];
+import Link from 'next/link';
+import styles from './Home.module.css';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      />
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+    <div className='flex flex-col items-center justify-center w-screen h-screen overflow-hidden'>
+      <h1 className='text-4xl font-semibold'>Vermenea</h1>
+      <p className='text-lg'>
+        This is a <span className={styles.rainbowText}>personal</span> portfolio.
+      </p>
+      <div className='flex space-x-4 mt-4'>
+        <Link href='https://github.com/vermenea' passHref>
+          <img src='/github.png' alt='github' className='h-12' />
+        </Link>
+        <Link href='https://www.linkedin.com/in/vermenea/' passHref>
+          <img src='/linkedin-logo.png' alt='linkedin' className='h-12' />
+        </Link>
+        <Link href='https://x.com/vermenea' passHref>
+          <img src='/twitter.png' alt='x' className='h-12' />
+        </Link>
+      </div>
     </div>
   );
 }
