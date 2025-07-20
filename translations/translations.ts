26397aedd941448006dab2885f9cf150b9c867f2
Natalia Zagórska <vermenea@gmail.com>
2025-07-20 23:35:39 +0100

    feat: add Dank Diary project to projects list; update translations for new project details

--- translations/translations.ts
+++ translations/translations.ts
@@ -3,75 +3,89 @@ export const translations = {
     nav: {
       home: 'Home',
       about: 'About',
-      projects: 'Projects'
+      projects: 'Projects',
     },
     home: {
       typingText: 'Hi, lets create something amazing',
-      contactMe: 'Contact me'
+      contactMe: 'Contact me',
     },
     about: {
       title: 'About me',
-      description: "Hello! My name is Natalia — I hold a degree in Medical Biology and you’ll most often find me working in a laboratory, but my second passion is web development. I take great pleasure in building the front end of web applications and crafting their prototypes. I have three years of professional experience and am constantly sharpening my skills.",
+      description:
+        'Hello! My name is Natalia — I hold a degree in Medical Biology and you’ll most often find me working in a laboratory, but my second passion is web development. I take great pleasure in building the front end of web applications and crafting their prototypes. I have three years of professional experience and am constantly sharpening my skills.',
       technologies: {
         title: 'Technologies',
         frontend: {
           title: 'Front-End',
-          description: 'TypeScript combined with React and Next.js enables creating scalable web applications with modern features and SEO optimization.'
+          description:
+            'TypeScript combined with React and Next.js enables creating scalable web applications with modern features and SEO optimization.',
         },
         styling: {
           title: 'Styling & UX',
-          description: 'I care about responsive and aesthetic design, using modern styling tools and prototyping in Figma and Photoshop.'
+          description:
+            'I care about responsive and aesthetic design, using modern styling tools and prototyping in Figma and Photoshop.',
         },
         tools: {
           title: 'Tools & Backend',
-          description: 'Git facilitates version control, and Node.js enables server creation. I use monitoring and CI/CD tools like Sentry, Postman, and Vercel, along with Firebase services.'
-        }
-      }
+          description:
+            'Git facilitates version control, and Node.js enables server creation. I use monitoring and CI/CD tools like Sentry, Postman, and Vercel, along with Firebase services.',
+        },
+      },
     },
     projects: {
-      title: "Projects",
+      title: 'Projects',
       goToProject: 'Go to project',
       checkOthers: 'Go check other projects (✿◡‿◡)',
       items: [
         {
           title: 'PomoBuddy - Project',
-          description: 'PomoBuddy is a Pomodoro Timer App designed to help individuals manage their work time effectively using the Pomodoro Technique. The Pomodoro Technique is a time management method that breaks work into intervals, traditionally 25 minutes in length, separated by short breaks. This technique aims to improve focus and productivity.'
+          description:
+            'PomoBuddy is a Pomodoro Timer App designed to help individuals manage their work time effectively using the Pomodoro Technique. The Pomodoro Technique is a time management method that breaks work into intervals, traditionally 25 minutes in length, separated by short breaks. This technique aims to improve focus and productivity.',
         },
         {
           title: 'Anita Maruszewska - Twoja uroda z Ajurwedą',
-          description: 'Freelance project.'
-        }
-      ]
-    }
+          description: 'Freelance project.',
+        },
+        {
+          title: 'Dank Diary - Project',
+          description:
+            'Dank Diary is a note-publishing application where users can post notes and thoughts on a post board. Users can create, edit, and delete their notes, as well as view notes from other users. The app is user-friendly and allows easy sharing of thoughts with others anonymously.',
+        },
+      ],
+    },
   },
   pl: {
     nav: {
       home: 'Strona główna',
       about: 'O mnie',
-      projects: 'Projekty'
+      projects: 'Projekty',
     },
     home: {
       typingText: 'Cześć, stwórzmy coś niesamowitego',
-      contactMe: 'Skontaktuj się ze mną'
+      contactMe: 'Skontaktuj się ze mną',
     },
     about: {
       title: 'O mnie',
-      description: "Cześć! Nazywam się Natalia – z wykształcenia jestem biologiem medycznym i na codzień można mnie spotkać w laboratorium ale moim drugim hobby jest tworzenie stron internetowych. Największą przyjemnność sprawia mi pisanie frontendu aplikacji webowych i tworzenie ich prototypów. Posiadam 3 letnie doświadczenie i nieustannie rozwijam swoje umiejętności.",
+      description:
+        'Cześć! Nazywam się Natalia – z wykształcenia jestem biologiem medycznym i na codzień można mnie spotkać w laboratorium ale moim drugim hobby jest tworzenie stron internetowych. Największą przyjemnność sprawia mi pisanie frontendu aplikacji webowych i tworzenie ich prototypów. Posiadam 3 letnie doświadczenie i nieustannie rozwijam swoje umiejętności.',
       technologies: {
         title: 'Technologie',
         frontend: {
           title: 'Front-End',
-          description: 'TypeScript w połączeniu z React i Next.js umożliwia tworzenie skalowalnych aplikacji webowych z nowoczesnymi funkcjami i optymalizacją SEO.'
+          description:
+            'TypeScript w połączeniu z React i Next.js umożliwia tworzenie skalowalnych aplikacji webowych z nowoczesnymi funkcjami i optymalizacją SEO.',
         },
         styling: {
           title: 'Stylowanie i UX',
-          description: 'Dbam o responsywny i estetyczny design, wykorzystując nowoczesne narzędzia stylizacyjne oraz prototypowanie w Figma i Photoshop.'
+          description:
+            'Dbam o responsywny i estetyczny design, wykorzystując nowoczesne narzędzia stylizacyjne oraz prototypowanie w Figma i Photoshop.',
         },
         tools: {
           title: 'Narzędzia i Backend',
-          description: 'Git ułatwia kontrolę wersji, a Node.js pozwala na tworzenie serwerów. Korzystam z narzędzi monitoringu i CI/CD, takich jak Sentry, Postman i Vercel, oraz z usług Firebase.'
-        }
-      }
+          description:
+            'Git ułatwia kontrolę wersji, a Node.js pozwala na tworzenie serwerów. Korzystam z narzędzi monitoringu i CI/CD, takich jak Sentry, Postman i Vercel, oraz z usług Firebase.',
+        },
+      },
     },
     projects: {
       title: 'Projekty',
@@ -80,13 +94,19 @@ export const translations = {
       items: [
         {
           title: 'PomoBuddy - Projekt',
-          description: 'PomoBuddy to aplikacja Pomodoro Timer zaprojektowana, aby pomóc użytkownikom efektywnie zarządzać czasem pracy przy użyciu Techniki Pomodoro. Technika Pomodoro to metoda zarządzania czasem, która dzieli pracę na interwały, tradycyjnie 25-minutowe, przedzielone krótkimi przerwami. Ta technika ma na celu poprawę koncentracji i produktywności.'
+          description:
+            'PomoBuddy to aplikacja Pomodoro Timer zaprojektowana, aby pomóc użytkownikom efektywnie zarządzać czasem pracy przy użyciu Techniki Pomodoro. Technika Pomodoro to metoda zarządzania czasem, która dzieli pracę na interwały, tradycyjnie 25-minutowe, przedzielone krótkimi przerwami. Ta technika ma na celu poprawę koncentracji i produktywności.',
         },
         {
           title: 'Anita Maruszewska - Twoja uroda z Ajurwedą',
-          description: 'Projekt freelancerski.'
-        }
-      ]
-    }
-  }
+          description: 'Projekt freelancerski.',
+        },
+        {
+          title: 'Dank Diary - Projekt',
+          description:
+            'Dank Diary to aplikacja do publikowania notatek, przemyśleń na tablicy postów. Użytkownicy mogą tworzyć, edytować i usuwać swoje notatki, a także przeglądać notatki innych użytkowników. Aplikacja jest prosta w obsłudze i pozwala na łatwe dzielenie się myślami z innymi anonimowo.',
+        },
+      ],
+    },
+  },
 };
